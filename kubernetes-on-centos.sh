d4ba3704b4f0f51ab43850aa195abcee20c3c346
As'ad Tahta Alfina <57313918+asadtahta95@users.noreply.github.com>
2022-08-29 12:09:19 +0700

    Update kubernetes-on-centos.sh

--- kubernetes-on-centos.sh
+++ kubernetes-on-centos.sh
@@ -1,34 +1,34 @@
 # Update packages
-yum update -y
+sudo yum update -y
 
 # Install Docker
 # Add repo
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 # remove podman dan buildah
-dnf remove podman buildah -y
+sudo dnf remove podman buildah -y
 # Install packages Docker
-dnf install docker-ce docker-ce-cli containerd.io -y
+sudo dnf install docker-ce docker-ce-cli containerd.io -y
 # Enable & start docker service
-systemctl start docker.service
-systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
 # Run docker without root
-usermod -aG docker $USER
+sudo usermod -aG docker $USER
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+sudo chmod +x ./kubectl
+sudomv ./kubectl /usr/local/bin/kubectl
 kubectl version
 
 # Install minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-mkdir -p /usr/local/bin/
+sudo chmod +x minikube
+sudo mkdir -p /usr/local/bin/
 install minikube /usr/local/bin
 minikube version
 minikube start --driver=docker
 
 # Install Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+sudo chmod 700 get_helm.sh
 ./get_helm.sh
